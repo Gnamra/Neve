d42c815cc3ba1c5e756bd61ae4c5480bc79b83a5
gnamra <gnamra@gmail.com>
2024-12-11 13:24:52 +0000

    revert previously commented code

--- config/telescope/telescope-nvim.nix
+++ config/telescope/telescope-nvim.nix
@@ -43,9 +43,9 @@
           colorscheme = {
             enable_preview = true;
           };
-           find_files = {
-             theme = "slate";
-           };
+          # find_files = {
+          #   theme = "slate";
+          # };
         };
       };
       keymaps = {
